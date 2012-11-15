759a434dca64d8064c75121c53d659018a83cb81
sarriaroman <agustin478@gmail.com>
2012-11-15 11:09:15 -0300

    Gzip deleted from static content

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.configure(function(){
     	})
     }));
     app.use(app.router);
-    //app.use(express.static(path.join(__dirname, 'public')));
-    app.use(gzippo.staticGzip(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    //app.use(gzippo.staticGzip(path.join(__dirname, 'public')));
 	app.use(gzippo.compress());
 });
 
