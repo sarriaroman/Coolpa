42627edde82a3e4f54b71201647bd7304ed596ba
sarriaroman <agustin478@gmail.com>
2012-12-24 14:40:45 -0300

    Images

--- routes/index.js
+++ routes/index.js
@@ -397,22 +397,28 @@ exports.mobile_message = function(req, res) {
             if( req.files.image.length > 0 ) {
                 var dirname = __dirname.replace('routes', '');
 
-                var name = (new Date()).getTime().toString(16) + '.jpg';
-                console.log(name);
-                imgs.push(name);
+                var name = (new Date()).getTime().toString(16);
+                console.log(name + '.jpg' );
+                imgs.push(name + '.jpg' );
 
                 var tmp = dirname + 'public/temp/' + name;
                 var easyimg = require('easyimage');
 
-                easyimg.convert({
-                    src: req.files.image.path, 
-                    dst: tmp, 
-                    quality:80
-                }, function(err, image) {
+                fs.readFile(req.files.image.path, function (err, data) {
                     if (err) throw err;
+                    
+                    fs.writeFile(tmp + '.png', new Buffer(data, base64).toString(), function(err) {
+                        easyimg.convert({
+                            src: tmp + '.png', 
+                            dst: tmp + '.jpg', 
+                            quality:80
+                        }, function(err, image) {
+                            if (err) throw err;
 
-                    s3.get().putFile(tmp, 'images/' + name, { 'x-amz-acl': 'public-read' }, function(err, rs){
-                        fs.unlink(tmp, function(){});
+                            s3.get().putFile(tmp + '.jpg', 'images/' + name, { 'x-amz-acl': 'public-read' }, function(err, rs){
+                                fs.unlink(tmp + '.jpg', function(){});
+                            });
+                        });
                     });
                 });
             }
