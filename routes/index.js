ed9ca9919c96dd269fbf606ddc748b7d6e17869a
sarriaroman <agustin478@gmail.com>
2012-12-31 14:03:00 -0300

    Tested factories... code cleaning

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 // Load factories
 var factories = require("./factories");
 
- exports.index = function(req, res){
+exports.index = function(req, res){
     if( req.session.uid == undefined ) {
 
         res.render('start', {
@@ -132,167 +132,6 @@ exports.mobile_push = function(req, res) {
         });
     });
 };
-
-/*var message_factory = function(req, res, information, callback) {
-    var Messages = require('../models/messages');
-    var users = new (require('../models/users'))();
-    var fs = require('fs');
-    var ses = new (require('../classes/ses'))();
-    var ejs = require('ejs');
-    
-    var Message = new Messages();
-    
-    var msg = information.message;
-    
-    if( msg.trim() == '' ) {
-        callback(req, res, information);
-        return;
-    }
-    
-    var ids = new Array();
-    msg.replace(/[:]+[A-Za-z0-9-_]+/g, function(u) {
-        var uname = u.replace(':', '').toLowerCase();
-        
-        if( uname.length > 2 ) {
-            if(ids.indexOf(uname) == -1) {
-                ids.push( uname );
-            }
-        }
-    });
-
-    // Process image and add to image array.
-    
-    Message.add({
-        message: msg,
-        ids: ids,
-        sender: information.uid,
-        public: information.public,
-        reply_to: information.reply_to,
-        author: information.author,
-        original_id: information.original_id,
-        hidden: false,
-        from: information.from,
-        images: information.images
-    }, function(msg, err) {
-        if( msg.public === true ) {
-            fs.readFile('views/email_template.html', 'UTF-8', function(err, html) {
-                // Prepare push message
-                var GCM = require('gcm').GCM;
-
-                var gcm = new GCM('AIzaSyBolUX8ziVmOUzK2wTA2lmR9MzdkPRjot8');
-
-                var gcm_message = {
-                    collapse_key: 'New Mentions', 
-                    'data.title': ':' + msg.sender + ' is talking about you on Coolpa',
-                    'data.message': msg.message
-                };
-
-                for( var i = 0 ; i < ids.length ; i++ ) {
-                    var uid = ids[i];
-                    console.log(uid);
-
-                    users.user(uid, function(err, data){
-                        if( data != undefined ) {
-                            // Mail Notifications
-                            if( data.notifications.mentions == 1 ) {
-                            ses.get().send({
-                                from: 'Coolpa.net <info@coolpa.net>',
-                                to: [data.email],
-                                subject: 'You have new mentions on Coolpa',
-                                body: {
-                                    html: ejs.render(html, {
-                                        username: data._id, 
-                                        uid: msg.sender
-                                    })
-                                }
-                            });
-                            console.log('Email sent to ' + data._id);
-                            }
-                            
-                            // Push notifications
-
-                            for( var di = 0 ; di < data.mobile.devices.length ; di++ ) {
-                                var dev = data.mobile.devices[di];
-
-                                if( dev.type == "Android" ) {
-                                    gcm_message.registration_id = dev.pid;
-
-                                    gcm.send(gcm_message, function(err, messageId){
-                                        if (err) {
-                                            console.log("Something has gone wrong! " + err);
-                                        } else {
-                                            console.log("Sent with message ID: ", messageId);
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    });
-                }
-
-            });
-        } else {
-            fs.readFile('views/private_template.html', 'UTF-8', function(err, html) {
-                // Prepare push message
-                var GCM = require('gcm').GCM;
-
-                var gcm = new GCM('AIzaSyBolUX8ziVmOUzK2wTA2lmR9MzdkPRjot8');
-
-                var gcm_message = {
-                    collapse_key: 'New Privates', 
-                    'data.title': ':' + msg.sender + ' sent you a new private message on Coolpa',
-                    'data.message': msg.message
-                };
-
-                for( var i = 0 ; i < ids.length ; i++ ) {
-                    var uid = ids[i];
-                    console.log(uid);
-                
-                    users.user(uid, function(err, data){
-                        if( data != undefined ) {
-                            if( data.notifications.privates == 1 ) {
-                            ses.get().send({
-                                from: 'Coolpa.net <info@coolpa.net>',
-                                to: [data.email],
-                                subject: 'You have new private messages on Coolpa',
-                                body: {
-                                    html: ejs.render(html, {
-                                        username: data._id, 
-                                        uid: msg.sender
-                                    })
-                                }
-                            });
-                            }
-                    
-                            // Push notifications
-
-                            for( var di = 0 ; di < data.mobile.devices.length ; di++ ) {
-                                var dev = data.mobile.devices[di];
-
-                                if( dev.type == "Android" ) {
-                                    gcm_message.registration_id = dev.pid;
-
-                                    gcm.send(gcm_message, function(err, messageId){
-                                        if (err) {
-                                            console.log("Something has gone wrong! " + err);
-                                        } else {
-                                            console.log("Sent with message ID: ", messageId);
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    });
-                }
-
-            });
-        }
-
-    callback(req, res, information);
-
-});
-
-};*/
 
 exports.mobile_newest = function(req, res) {
     factories.mobile_security(req, res, function(request, response){
@@ -554,20 +393,6 @@ exports.connect = function(req, res) {
     });
 };
 
-/*var conversation_factory = function(msgs, id, request, response, callback) {
-    var message = new (require('../models/messages'))();
-
-    message.get(id, function(err, data) {
-        msgs.push( data );
-
-        if( data.reply_to == '' || data.reply_to == -1 ) {
-            callback(msgs, request, response);
-        } else {
-            conversation_factory(msgs, data.reply_to, request, response, callback);
-        }
-    });
-};*/
-
 exports.conversation = function(req, res) {
     if( req.session.uid == undefined ) {
         res.redirect('/');
@@ -708,43 +533,6 @@ var date_sort_desc = function (date1, date2) {
   return 0;
 };
 
-/*var home_factory = function(session_uid, callback) {
-    var messages = new (require('../models/messages'))();
-    var users = new (require('../models/users'))();
-
-    users.user( session_uid, function(err, data) {
-        messages.find( session_uid, data.connections.slice(0), new Date(), function(err, docs) {
-            messages.count( session_uid, [], function(err, cnt) {
-                users.connections( session_uid, function(err, conns) {
-
-                    var autocomplete = "[";
-                    for( var i = 0 ; i < data.connections.length ; i++ ) {
-                        var obj = "{'id':'" + data.connections[i] + "', 'name':':" + data.connections[i] + "', 'avatar': '/avatars/" + data.connections[i] + "/avatar.square.jpg" + "', 'icon':'" + "', 'type':'contact'}";
-
-                        if( i < (data.connections.length - 1) ) {
-                            obj += ",";
-                        }
-
-                        autocomplete += obj;
-                    }
-                    autocomplete += "]";
-
-                    callback({
-                        user: data._id,
-                        username: '',
-                        messages: docs,
-                        count: cnt,
-                        connections: data.connections.length,
-                        connecteds: conns.length,
-                        autocomplete: autocomplete,
-                        section: 'start'
-                    });
-                }); 
-            } );
-        });
-    } );
-};*/
-
 exports.mobile_start = function(req, res) {
     factories.mobile_security(req, res, function(request, response){
         factories.home_factory(request.body.username, function(data){
@@ -794,30 +582,6 @@ exports.start = function(req, res) {
         } );
     }
 };
-
-/*var privates_factory = function(session_uid, callback) {
-    var messages = new (require('../models/messages'))();
-    var users = new (require('../models/users'))();
-
-    users.user( session_uid, function(err, data) {
-        messages.privates( session_uid, new Date(), function(err, docs) {
-            messages.count(session_uid, [], function(err, cnt) {
-                users.connections( session_uid, function(err, conns) {
-
-                    callback({
-                        user: session_uid,
-                        username: '',
-                        messages: docs, // Reversing array
-                        count: cnt,
-                        connections: data.connections.length,
-                        connecteds: conns.length,
-                        section: 'privates'
-                    });
-                }); 
-            } );
-        });
-    } );
-};*/
 
 exports.mobile_privates = function(req, res) {
     factories.mobile_security(req, res, function(request, response){
@@ -897,43 +661,6 @@ exports.more = function(req, res) {
         } );
     }
 };
-
-/*var mentions_factory = function(session_uid, callback) {
-    var messages = new (require('../models/messages'))();
-    var users = new (require('../models/users'))();
-
-    users.user( session_uid, function(err, data) {
-        messages.mentions( session_uid, new Date(), function(err, docs) {
-            messages.count( session_uid, [], function(err, cnt) {
-                users.connections( session_uid, function(err, conns) {
-
-                    var autocomplete = "[";
-                    for( var i = 0 ; i < data.connections.length ; i++ ) {
-                        var obj = "{'id':'" + data.connections[i] + "', 'name':':" + data.connections[i] + "', 'avatar': '/avatars/" + data.connections[i] + "/avatar.square.jpg" + "', 'icon':'" + "', 'type':'contact'}";
-
-                        if( i < (data.connections.length - 1) ) {
-                            obj += ",";
-                        }
-
-                        autocomplete += obj;
-                    }
-                    autocomplete += "]";
-                    
-                    callback({
-                        user: session_uid,
-                        username: '',
-                            messages: docs, // Reversing array
-                            count: cnt,
-                            connections: data.connections.length,
-                            connecteds: conns.length,
-                            autocomplete: autocomplete,
-                            section: 'mentions'
-                        });
-                }); 
-            } );
-        });
-    } );
-};*/
 
 exports.mobile_mentions = function(req, res) {
     factories.mobile_security(req, res, function(request, response){
@@ -1534,20 +1261,6 @@ exports.showmessage = function(req, res) {
         });
     }
 };
-
-/*var remove_factory = function(mid, uid, req, res, callback) {
-    var messages = new (require('../models/messages'))();
-        
-    messages.get(mid, function(err, data) {
-        if( data.sender == uid ) {
-            messages.remove(mid, function(err) {
-                callback(true, req, res);
-            });
-        } else {
-            callback(false, req, res);
-        }
-    });
-};*/
 
 exports.remove_message = function(req, res) {
     if( req.session.uid == undefined ) {
