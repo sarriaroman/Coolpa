8f83133bdf4e8a15b5c03315abaac59ff66c78ed
sarriaroman <agustin478@gmail.com>
2013-01-03 23:44:19 -0300

    Avoid redirections

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.configure(function(){
     	})
     }));
     app.use(app.router);
+    app.use(express.compress());
     app.use(express.static(path.join(__dirname, 'public')));
-	app.use(express.compress());
 });
 
 app.configure('development', function(){
