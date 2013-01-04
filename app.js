b2e03807fe768908704981ce1c8a0f20c011e093
sarriaroman <agustin478@gmail.com>
2013-01-04 18:25:15 -0300

    API Subdomain & Compression

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ app.configure(function(){
 });
 
 // Subdomain processor.
-app.all('*', function(req, res, next){ 
+app.all('*', function(req, res, next){
+    if(req.headers.host.indexOf('www') != -1) res.redirect('coolpa.net');
+    
     if(req.headers.host == 'api.coolpa.net' || req.headers.host == 'api.coolpa.net:3001')
         req.url = '/api_subdomain' + req.url;
     
