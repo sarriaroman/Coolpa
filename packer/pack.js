130235c41242ef67bcb36ee290722629e9a9a3b8
sarriaroman <agustin478@gmail.com>
2013-01-04 02:14:40 -0300

    Packer

--- packer/pack.js
+++ packer/pack.js
@@ -23,12 +23,10 @@ if( type == 'html' ) {
   				var html_packed = S(data).s; //packer.packHTML(S(data).s);
   				html_packed = html_packed.collapseWhitespace();
 
-  				console.log(html_packed);
-
-				/*fs.writeFile('../views/' + files[i], html_packed, function() {
+				fs.writeFile('../views/' + files[i], html_packed.s, function() {
 					if (err) throw err;
   					console.log('It\'s saved!');
-				});*/
+				});
 			});
 		}
 	});
