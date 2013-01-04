efea885ed3c563763130edadecfea381396ad20e
sarriaroman <agustin478@gmail.com>
2013-01-03 23:38:40 -0300

    Avoid redirections

--- routes/index.js
+++ routes/index.js
@@ -211,15 +211,31 @@ exports.create_images = function(req, res) {
 
 exports.avatars = function(req, res) {
     var users = new (require('../models/users'))();
+    var s3 = new (require('../classes/s3'))();
 
     users.user(req.params.username, function(err, data) {
+        var image = "";
+
         if( req.params.file == 'avatar.original.jpg' ) {
-            res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.original );
+            //res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.original );
+            image = data.images.original;
         } else if( req.params.file == 'avatar.square.jpg' ) {
-            res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.square );
+            //res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.square );
+            image = data.images.square;
         } else {
-            res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.top );
+            //res.redirect('https://coolpa.s3.amazonaws.com/' + data.images.top );
+            image = data.images.top;
         }
+
+        s3.get().get('/images/' + req.params.file).on('response', function(response){
+            res.set(response.headers);
+            response.on('data', function(chunk){
+                res.write(chunk);
+            });
+            response.on('end', function() {
+                res.end();
+            });
+        }).end();
     });    
 };
 
