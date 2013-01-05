2647fbe8cb33252941eecd9b8b9bfa384d2a34c2
sarriaroman <agustin478@gmail.com>
2013-01-05 17:03:17 -0300

    Packer improvements

--- packer/pack.js
+++ packer/pack.js
@@ -6,38 +6,33 @@
 var S = require('string');
 S.extendPrototype();
 
-// Check
-var proto_test = ' Trim proto ';
-console.log( proto_test.trim() );
+console.warn('Starting resource compression');
+console.warn('This will take a while');
 
 var fs = require('fs'),
 	packer = require('./packer');
 
-var type = process.env.TYPE.toLowerCase();
-if( type == 'html' ) {
-	/* Pack HTML */
-	fs.readdir('../original_views/', function(err, files){
-		console.log(files);
-		for( var i = 0 ; i < files.length ; i++ ) {
-			console.log("Current: " + files[i]);
+/* Pack HTML */
+fs.readdir('../original_views/', function(err, files){
+	for( var i = 0 ; i < files.length ; i++ ) {
+		console.log("Current: " + files[i]);
 
-			var html_packed = S( fs.readFileSync('../original_views/' + files[i]) ).s;
+		var html_packed = S( fs.readFileSync('../original_views/' + files[i]) ).s;
   			
-  			html_packed = html_packed.replace(/(\r\n|\r|\n)/g, '');
-  			html_packed = html_packed.replace(/<!--[\s\S]*?-->/g, '');
-  			html_packed = html_packed.collapseWhitespace();
+  		html_packed = html_packed.replace(/(\r\n|\r|\n)/g, '');
+  		html_packed = html_packed.replace(/<!--[\s\S]*?-->/g, '');
+  		html_packed = html_packed.collapseWhitespace();
 
-  			console.log(html_packed.s);
+		fs.writeFileSync('../views/' + files[i], html_packed.s);
+	}
+});
 
-			fs.writeFileSync('../views/' + files[i], html_packed.s);
-		}
-	});
-} else if( type == 'css' ) {
-	/* Pack CSS */
-	//var css_packed = packer.packCSS("send your css content to pack") ;
-	console.error('Unsupported');
-} else if( type == 'js' ) {
-	/* Pack JS */
-	//var js_packed = packer.packJS("send your JS content to pack here", "optionnaly send the path of the file for better error reporting");
-	console.error('Unsupported');
-}
+console.info('Processing CSS');
+/* Pack CSS */
+//var css_packed = packer.packCSS("send your css content to pack") ;
+console.error('Unsupported');
+
+console.info('Processing JS');
+/* Pack JS */
+//var js_packed = packer.packJS("send your JS content to pack here", "optionnaly send the path of the file for better error reporting");
+console.error('Unsupported');
