d35dfaf494fa47fbd8336356f60576d5b7abbae5
sarriaroman <agustin478@gmail.com>
2013-01-07 13:53:20 -0300

    Packer improvements

--- packer/functions.js
+++ packer/functions.js
@@ -1,8 +1,11 @@
 exports.rmDir = function(dirPath) {
 	var fs = require('fs');
-	
+
 	try { var files = fs.readdirSync(dirPath); }
-	catch(e) { return; }
+	catch(e) { 
+		console.trace(e);
+		return; 
+	}
 	if (files.length > 0)
 		for (var i = 0; i < files.length; i++) {
 			var filePath = dirPath + '/' + files[i];
